d3edbb7c76ee974d2d92b24ce933f153ee72b608
ahirandrakasmi <andra201811.gmail.com>
2025-05-31 11:28:19 +0700

    uas-selesai

--- lib/data_saya.dart
+++ lib/data_saya.dart
@@ -1,4 +1,4 @@
 class DataSaya {
-   static String nama = "Kudut";
-    static String gambar = "assets/mahasiswa/kudut.png";
+   static String nama = "Ahirandra Kasmi";
+    static String gambar = "assets/gambar_dosen/Ahirandra_Kasmi.jpg";
 }
--- lib/screen/beranda_screen.dart
+++ lib/screen/beranda_screen.dart
@@ -1,8 +1,33 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:whazlansaja/screen/pesan_screen.dart';
 
-class BerandaScreen extends StatelessWidget {
+class BerandaScreen extends StatefulWidget {
   const BerandaScreen({super.key});
+
+  @override
+  State<BerandaScreen> createState() => _BerandaScreenState();
+}
+
+class _BerandaScreenState extends State<BerandaScreen> {
+  List<dynamic> dosenList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadDosenData();
+  }
+
+  Future<void> loadDosenData() async {
+    final String response =
+        await rootBundle.loadString('assets/json_data_chat_dosen/dosen_chat.json');
+    final List<dynamic> data = json.decode(response);
+    setState(() {
+      dosenList = data;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +35,7 @@ class BerandaScreen extends StatelessWidget {
       appBar: AppBar(
         elevation: 2,
         title: const Text(
-          'WhAzlansaja',
+          'WhKasmisaja',
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.bold,
@@ -41,20 +66,72 @@ class BerandaScreen extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-        itemCount: 40,
+        itemCount: dosenList.length,
         itemBuilder: (context, index) {
+          final dosen = dosenList[index];
+          final List<dynamic> messages = dosen['details'] ?? [];
+          final lastMessage = messages.isNotEmpty
+          ? messages[messages.length - 1]['message']
+          : 'Belum ada chat';
+          final String lastMessageRole = messages.isNotEmpty
+          ? messages[messages.length - 1]['role']
+          : '';
+          final String lastMessageTime = lastMessageRole == 'dosen' ? '2 menit lalu' : 'Kemarin';
+
+          // Since JSON does not have 'is_read', unreadCount will be 0
+          final int unreadCount = lastMessageRole == 'dosen' ? 1 : 0;
+
           return ListTile(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const PesanScreen()));
-            },
-            leading: const CircleAvatar(
-              backgroundImage:
-                  AssetImage('assets/gambar_dosen/Azlan, S.Kom., M.Kom.jpg'),
+        onTap: () {
+          // Transform dosen data to match PesanScreen expected structure
+          final transformedDosen = {
+            'avatar': dosen['img'],
+            'full_name': dosen['name'],
+            'messages': (dosen['details'] as List<dynamic>).map((detail) {
+              return {
+                'from': detail['role'] == 'dosen' ? 0 : 1,
+                'message': detail['message'],
+              };
+            }).toList(),
+          };
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+          builder: (context) => PesanScreen(dosen: transformedDosen),
             ),
-            title: const Text('Azlan'),
-            subtitle: const Text('Belum ada chat'),
-            trailing: const Text('Kemaren'),
+          );
+        },
+        leading: CircleAvatar(
+          backgroundImage: AssetImage(dosen['img']),
+        ),
+        title: Text(dosen['name']),
+        subtitle: Text(lastMessage),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (unreadCount > 0)
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(
+              unreadCount.toString(),
+              style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+            const SizedBox(height: 4),
+            Text(
+          lastMessageTime,
+          style: const TextStyle(fontSize: 12),
+            ),
+          ],
+        ),
           );
         },
       ),
